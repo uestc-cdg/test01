b971318af579e1638e8262391a9f85ca2a2305f9
hello_cdg <875847402@qq.com>
2025-03-13 13:16:16 +0800

    is finish

---
+++ kitty.cpp
@@ -0,0 +1,16 @@
+#include "kitty.h"
+
+void kitty::cute()
+{
+    std::cout << "你煞笔吗！" << std::endl;
+}
+
+void kitty::eat()
+{
+    std::cout << "I am hungry!" <<std::endl;
+}
+
+void kitty::sleep()
+{
+    std::cout << "哥们儿，我有点困了呀！" << std::endl;
+}
